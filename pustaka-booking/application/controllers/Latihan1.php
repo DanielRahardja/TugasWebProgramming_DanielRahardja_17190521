0d3301e9621f06723340eabcc7e90db51f41761b
DanielRahardja <danielrahardja4@gmail.com>
2022-04-07 08:47:23 +0700

    Modifikasi file latihan1.php

--- pustaka-booking/application/controllers/Latihan1.php
+++ pustaka-booking/application/controllers/Latihan1.php
@@ -3,4 +3,11 @@ class Latihan1 extends CI_Controller{
     public function index(){
         echo "Selamat Datang... Selamat belajar web programming";
     }
+
+    public function penjumlahan($n1,$n2){
+        $this->load->model("M_Latihan1");
+        $hasil= $this->M_Latihan1->jumlah($n1,$n2);
+
+        echo "Hasil Penjumlahannya dari ".$n1."+".$n2.="=".$hasil;
+    }
 }
