68cf54301fd18c7a0bed8034b0d1478307a9ebde
DanielRahardja <danielrahardja4@gmail.com>
2022-04-07 08:54:12 +0700

    Modifikasi tambahan untuk file latihan1.php

--- pustaka-booking/application/controllers/Latihan1.php
+++ pustaka-booking/application/controllers/Latihan1.php
@@ -5,7 +5,7 @@ class Latihan1 extends CI_Controller{
     }
 
     public function penjumlahan($n1,$n2){
-        $this->load->model("M_Latihan1");
+        $this->load->model('M_Latihan1');
         $hasil= $this->M_Latihan1->jumlah($n1,$n2);
 
         echo "Hasil Penjumlahannya dari ".$n1."+".$n2.="=".$hasil;
---
+++ pustaka-booking/application/views/v_latihan.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    Halo,semuanya! Yuk kita belajar web programming 2!<br>
+    Nilai1= <?= $nilai1; ?>
+    Nilai2= <?= $nilai2; ?>
+
+    Hasilnya dengan metode penjumlahan adalah: <?= $nilai1 ."+".$nilai2."=".$hasil; ?> 
+</body>
+</html>
