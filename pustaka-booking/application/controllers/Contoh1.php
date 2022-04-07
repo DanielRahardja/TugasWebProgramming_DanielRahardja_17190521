b8f3022d3f22f3ec2ed5a4e8036f67c633e291cb
DanielRahardja <danielrahardja4@gmail.com>
2022-04-07 08:11:18 +0700

    Menambahkan file Contoh1.php

---
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php 
+class Contoh1 extends CI_Controller{
+    public function index(){
+        echo "<h1>PERKENALKAN</h1>";
+        echo "<h1>NAMA SAYA DANIEL RAHARDJA<br/> 
+        Saya tinggal di daerah BSD<br/>
+        Olahraga yang saya sukai adalah Menembak</h1>";
+    }
+}
