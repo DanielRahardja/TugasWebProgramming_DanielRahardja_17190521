7963ecb6041b9c3059a2e8b916a01a7bc036fe8e
DanielRahardja <danielrahardja4@gmail.com>
2022-04-14 11:19:07 +0700

    Membuat templating web sederhana

---
+++ pustaka-booking/application/views/v_index.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p align='justify'>Pada pengertian codeigniter di atas tadi dijelaskan bahwa codeigniter menggunakan metode 
+        MVC. Apa itu MVC? Kita akan mengetahui apa itu MVC sebelum masuk dan lebih 
+        jauh dalam belajar codeigniter.</p>
+    <p>MVC adalah teknik atau konsep yang memisahkan komponen utama menjadi 3 komponen yaitu: Model, View dan
+        Controller.</p>
+    <ol type="a">
+        <li>Model</li>
+    <p align='justify'>Model adalah kelas yang merepresentasikan atau memodelkan tipe data yang akan digunakan
+        oleh aplikasi. Model juga dapat didefinisikan sebagai penanganan yang berhubungan dengan pengolahan atau 
+        manipulasi Database.</p>
+        <li>View</li>
+    <p align='justify'>View merupakan bagian yang menangani halaman user interface/UI atau halaman yang muncul
+        pada user(Di Browser). Tampilan dari user interface(UI) dikumpulkan pada view untuk memisahkannya menggunakan
+        controller dan model dengan tujuan untuk mempermudah web designer atau programmer dalam pengembangan aplikasi
+        web.</p>
+        <li>Controller</li>
+    <p align='justify'>Controller merupakan kumpulan instruksi atau command yang menghubungkan model dan view. Jadi
+        data akan tersimpan di database(model) diambil oleh controller dan controller yang menampilkannya ke view.
+    </p>
+    <p align='justify'>Dari penjelasan tentang MVC di atas kita dapat menyimpulkan bahwa model yang bertugas sebagai
+        pengambilan data dari database, view yang bertugas untuk menangani penampilan halaman pada user dan 
+        controller yang bertugas sebagai pengolah instruksi dan meneruskan instruksi ke view.</p>
+</section>
